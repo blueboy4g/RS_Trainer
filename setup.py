c196905ed46d72fc01b6a781a219a26410362e36
blueboy4g <blueboy4g@hotmail.com>
2025-04-08 19:17:24 -0500

    Adding Magic telos test

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='RS_Trainer',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='PC',
+    author_email='',
+    description=''
+)
